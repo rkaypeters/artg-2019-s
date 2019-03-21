ca0c7717f87d16718a9df720918d5ea3a6792dca
Rachel <rkaypeters@gmail.com>
2019-03-20 20:25:57 -0400

    after week 8

--- examples/dispatch/script.js
+++ examples/dispatch/script.js
@@ -1,65 +1,72 @@
 const redSquare = d3.select('.main')
-    .append('div')
-    .attr('class','square')
-    .style('background','red')
-    .datum({key:3});
+	.append('div')
+	.attr('class','square')
+	.style('background','red')
+	.datum({key:4})
 
 redSquare
-    .on('click',function(d){
-        //'event listener'
-        console.log('Red square is clicked');
-        console.log(this); //'this' -> context of the function -> 'owner'
-        console.log(d);
-        console.log(d3.event);
-        //d3.select(this).transition().style('background','blue')//generally not encouraged
-});
+	.on('click', function(d){
+		//"event listener"
+		console.log('Red square is clicked');
+		console.log(this); //"this" --> context of the function --> "owner" of the function
+		console.log(d);
+		console.log(d3.event);
+	});
 
-/*const blueSquare = d3.select('.main')
-    .append('div')
-    .attr('class','square')
-    .style('background','blue')
-    .on('click',function(d){
-        console.log('Blue square is clicked');
-        console.log(this);
-    });
+//Create a dispatch object
+//it's going to handle the "change color" event
+const dispatch = d3.dispatch("change:color", "update:position")
 
-const yellowSquare = redSquare
-    .append('div','square')
-    .attr('class','square')
-    .style('background','yellow')
-    .on('click',function(d){
-        d3.event.stopPropagation();
-        console.log('Yellow square is clicked');
-        console.log(this);
-        console.log(d3.event);
-    })*/
+for(let i = 0; i < 10; i++){
+	const randomColor = `rgb(${255*Math.random()}, ${255*Math.random()}, ${255*Math.random()})`
 
-//create a dispatch object that hanles the 'change color' event
-const dispatch = d3.dispatch("change:color")//naming is up to you and this can also hand additional events, separated by a comma
+	const square = d3.select('.main')
+		.append('div')
+		.attr('class','square')
+		.style('background', randomColor)
+		.on('click', () => dispatch.call("change:color", null, randomColor));
 
-
-for (i=0;i<10;i++){
-    const randomColor = `rgb(${255*Math.random()},${255*Math.random()},${255*Math.random()})`
-    
-    const square = d3.select('.main')
-        .append('div')
-        .attr('class','square')
-        .style('background',randomColor)
-        //.on('click',function(){
-           //dispatch.call("change:color", randomColor) 
-        //});
-        .on('click',() => dispatch.call("change:color",null,randomColor));
-    
-    dispatch.on("change:color." + i,function(color){
-        console.log("change color");
-        square.style('background',color);
-    })
+	dispatch.on('change:color.'+i, function(color){
+		console.log('change color');
+		square.style('background', color);
+	});
 }
 
-dispatch.on("change:color",function(color){
-    console.log(color);
-    
+dispatch.on('change:color', function(color){
+	console.log(color);
 })
 
+//Second example, demonstrating the necessity of the dispatch pattern
+const svg = d3.select('.main')
+	.append('svg')
+	.attr('width',960)
+	.attr('height',600)
+	.style('padding',0)
+	.style('margin',0);
 
+const btn = d3.select('.main')
+	.insert('button','svg')
+	.on('click', () => generateNewShape(svg))
+	.html('Add new shape');
 
+function generateNewShape(rootDOM){
+	const color = `rgb(${255*Math.random()}, ${255*Math.random()}, ${255*Math.random()})`;
+	const x = Math.random()*960;
+	const y = Math.random()*600;
+	let el;
+
+	if(Math.random()>.5){
+		el = rootDOM.append('circle')
+			.attr('r',10)
+			.style('fill',color);
+	}else{
+		el = rootDOM.append('rect')
+			.attr('width',20)
+			.attr('height',20)
+			.style('fill',color);
+	}
+	el.attr('transform', `translate(${x},${y})`)
+		.on('click', () => dispatch.call("change:color",null,color));
+	dispatch.on('change:color.'+Math.floor(Math.random()*10000), color => el.style('fill', color));
+
+}
---
+++ week-8/canvas/script.js
@@ -0,0 +1,140 @@
+//Prepare DOM canvas
+const w = d3.select('.plot').node().clientWidth;
+const h = d3.select('.plot').node().clientHeight;
+const plot1 = d3.select('#plot-1')
+	.style('position','relative')
+	.append('svg').attr('width',w).attr('height',h);
+const plot1Canvas = d3.select('#plot-1')
+	.append('canvas').attr('width',w).attr('height',h)
+	.style('position','absolute')
+	.style('top',0)
+	.style('left',0);
+const plot2 = d3.select('#plot-2')
+	.style('position','relative')
+	.append('svg').attr('width',w).attr('height',h);
+const plot2Canvas = d3.select('#plot-2')
+	.append('canvas').attr('width',w).attr('height',h)
+	.style('position','absolute')
+	.style('top',0)
+	.style('left',0);
+
+//Dummy data
+const NUM_NODES = 1000;
+const nodesData = Array.from({length:NUM_NODES}).map(d => {
+	return {
+		x: Math.random()*w,
+		y: Math.random()*h,
+		vx: (Math.random()-.5)*.9,
+		vy: (Math.random()-.5)*.9,
+		r: 3,
+		color: `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+	}
+});
+
+//EXERCISE 1: Draw basic shapes
+//Access the drawing context of the canvas element; "drawing context" == "pen"
+const ctx1 = plot1Canvas.node().getContext('2d');
+
+//Drawing shapes 
+//Rect
+ctx1.fillStyle = 'rgb(255,0,0)';
+ctx1.fillRect(0,0,w/2,h/2);
+ctx1.fillStyle = 'white';
+ctx1.fillRect(0,0,w,h);
+ctx1.strokeStyle = 'black';
+ctx1.strokeRect(0,0,w/2,h/2);
+console.log(ctx1);
+
+//Path
+//ctx.beginPath()
+ctx1.beginPath();
+
+//path methods
+//ctx.moveTo()
+//ctx.lineTo()
+//ctx.arc()
+//ctx.quadraticCurveTo()
+//ctx.bezierCurveTo()
+ctx1.moveTo(w/2,h/2);
+ctx1.lineTo(w,h/2);
+ctx1.lineTo(w/2,h);
+
+//ctx.closePath()
+ctx1.closePath();
+ctx1.fillStyle = 'yellow';
+ctx1.fill();
+
+ctx1.beginPath();
+ctx1.moveTo(w/2+50,h/2);
+ctx1.arc(w/2,h/2,50,0,Math.PI*2);
+ctx1.closePath();
+ctx1.fillStyle = 'green';
+ctx1.fill();
+ctx1.strokeStyle = 'red';
+ctx1.stroke();
+
+//Arbitrary shape
+ctx1.beginPath();
+ctx1.moveTo(0,h);
+ctx1.quadraticCurveTo(w/2,h/2,w,h);
+ctx1.closePath();
+ctx1.fillStyle = 'purple';
+ctx1.fill();
+
+nodesData.forEach(d => {
+    ctx1.beginPath();
+    
+    ctx1.moveTo(d.x,d.y);
+    ctx1.arc(d.x,d.y,d.r,0,Math.PI*2);
+    ctx1.closePath();
+    ctx1.fillStyle = d.color;
+    ctx1.fill();
+})
+
+
+//ctx.stroke()
+//ctx.fill()
+
+//EXERCISE 2: use d3.path() to render path commands to <svg>
+
+//EXERCISE 3: animation and performance
+renderFrame();
+
+function renderFrame(){
+    
+    //update nodesData slightly
+    nodesData.forEach(d =>{
+        d.x += d.vx;
+        d.y += d.vy;
+        
+    if(d.x < 0){
+        d.x = w;
+    }
+    if(d.x > w){
+        d.x = 0;
+    }
+    if(d.y < 0){
+        d.y = h;
+    }
+    if(d.y > h){
+        d.y = 0;
+    }
+    
+    ctx1.beginPath();
+    
+    ctx1.moveTo(d.x,d.y);
+    ctx1.arc(d.x,d.y,d.r,0,Math.PI*2);
+    ctx1.closePath();
+    ctx1.fillStyle = d.color;
+    ctx1.fill();     
+        
+    });
+    
+    
+    requestAnimationFrame(renderFrame);
+        
+
+}
+
+
+
