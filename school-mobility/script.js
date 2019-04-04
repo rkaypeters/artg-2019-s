ef3371e2f994bbeb2712d23d311ef4d4d4ca26bc
Rachel <rkaypeters@gmail.com>
2019-04-03 20:59:14 -0400

    updated node/link structure

--- school-mobility/script.js
+++ school-mobility/script.js
@@ -1,7 +1,13 @@
+/// Promises for csv's
+
 const mobstabdataPromise = d3.csv('/school-mobility/data/mobstab18/school.csv',parseMobstab);
 const metadataPromise = d3.csv('/school-mobility/data/sch_metadata.csv',parseMetadata);
 const geodataPromise = d3.csv('/school-mobility/data/RI_Schools_coordinates_Mar2019.csv',parseGeodata);
 const schEntersPromise = d3.csv('/school-mobility/data/sch_enters_data_0809_1718.csv',parseEnterdata);
+
+
+
+//// Parse functions
 
 function parseEnterdata(d){
     return{
@@ -86,6 +92,10 @@ function parseGeodata(d){
     delete d.updated_by;
 }
 
+
+
+//// The bulk of it - after the promises
+
 Promise.all([
     mobstabdataPromise,metadataPromise,geodataPromise,schEntersPromise]).then(([mobstab,metadataSch,geodata,entersdata]) => {
                                               
@@ -94,71 +104,56 @@ Promise.all([
     //console.log(geodata);
     //console.log(entersdata);
     
-    const admin_tmp = metadataSch.map(d => {
+    const meta_tmp = metadataSch.map(d => {
 				return [d.schcode, d]
 			});
-    const adminMap = new Map(admin_tmp);
-    //console.log(adminMap);
+    const metaMap = new Map(meta_tmp);
+    //console.log(metaMap);
+    
+    myProjection(d3.select('.network').node(),geodata); //still need to set up network DOM dimentions
+    //console.log(geodata);
+    
+    const geo_tmp = geodata.map(d => {
+        return[d.schcode,d]
+    });
+    const geoMap = new Map(geo_tmp);
+    //console.log(geoMap);
+    
+    //console.log(entersdata);
     
     const enters1718 = entersdata.filter(d => d.reportID ==77)
         .filter(d => d.schcode_dest != ' ')
-        .filter(d => d.schcode_origin != d.schcode_dest);
+        .filter(d => d.schcode_origin != d.schcode_dest)
+        .map(d => {
+        const md = metaMap.get(d.schcode_dest);
+        d.adminSite_dest = md.adminSite;
+        d.distcode_dest = md.distcode;
+        d.schname30_dest = md.schname30;
+        return d;
+    })
+        .filter(d => d.adminSite_dest == 'N')
+        .map(d => {
+            const gd = geoMap.get(d.schcode_dest);
+            if(gd){
+                d.lngLat_dest = gd.lngLat;
+                d.xy_dest = gd.xy;
+            }
+            const go = geoMap.get(d.schcode_origin);
+            if(go){
+                d.lngLat_origin = go.lngLat;
+                d.xy_origin = go.xy;
+            }
+            return d;
+            });
+        
     console.log(enters1718);
     
-    const nodesData18 = new Map();
-    const linksData18 = [];
-    
-    enters1718.forEach(d => {
-        const newLink ={
-            value:d.enters
-        }
-        
-        if(!nodesData18.get(d.schcode_dest)){ //undefined is false
-            const newNode = {
-                schcode: d.schcode_dest
-                //incoming: [newLink],
-                //incomingTotal: newLink.value
-            };
-            
-            nodesData18.set(d.schcode_dest, newNode); //add it to the map
-            newLink.dest = newNode
-            ;
-        }else{
-            const existingNode = nodesData18.get(d.schcode_dest);
-            //existingNode.schcode = d.schcode_dest,
-            //existingNode.incoming.push(newLink);
-            //existingNode.incomingTotal += newLink.value;
-            newLink.dest = existingNode;
-        }
-        
-        if(!nodesData18.get(d.schcode_origin)){
-            const newNode = {
-                schcode: d.schcode_origin
-            };
-            
-            nodesData18.set(d.schcode_origin, newNode);
-            newLink.origin = newNode;
-        }else{
-            const existingNode = nodesData18.get(d.schcode_origin);
-            //existingNode.schcode = 
-            newLink.origin = existingNode;
-        }
-        
-        
-        linksData18.push(newLink);
-        
-        //if present, then modify the existing node
-        //if not present, create a new node
-    })
-    
-    console.log(nodesData18);
-    console.log(linksData18);
+
     
     const mobstab_sch = mobstab
         .filter(d => d.schname != '')
         .map(d => {
-        //console.log(adminMap.get(d.schcode));
-        const md = adminMap.get(d.schcode);
+        const md = metaMap.get(d.schcode);
         d.adminSite = md.adminSite;
         
         return d;
@@ -168,71 +163,72 @@ Promise.all([
     //console.log(mobstab_sch);
     drawBarChart(d3.select('.overview').node(), mobstab_sch);
     drawMap(d3.select('.map').node(),geodata);
+    
+    const nodesData = networkSetup(enters1718)[0];
+    const linksData = networkSetup(enters1718)[1]; //doesn't seem like the most efficient way to do this, but seems to work...
+    
+    console.log(nodesData);
+    console.log(linksData);
+    
+    
 }
     
 )
 
 
-//Converts origin destination flows to nodes and links
-/*function generateNetwork(data, year){
-    
-    let filteredData = data.filter(d => d.year === year);
+// broke the prep of nodes and links out into its own function
+
+function networkSetup(data){
     
     const nodesData = new Map();
     const linksData = [];
     
-    filteredData.forEach(od => {
-        //console.log(od);
+    data.forEach(d => {
+        const newLink = {
+            value: d.enters
+        };
         
-        const newLink ={
-            value: od.value
-        }
-        
-        //check to see if node is already in the nodesData map
-        //! or if makes non-undefined values true
-        if(!nodesData.get(od.origin_name)){ //undefined is false
+        if(!nodesData.get(d.schcode_dest)){
             const newNode = {
-                name: od.origin_name,
-                incoming: [],
-                outgoing: [newLink],
-                incomingTotal: 0,
-                outgoingTotal: newLink.value
+                schcode: d.schcode_dest,
+                xy: d.xy_dest,
+                totalEnters: newLink.value
             };
-            
-            nodesData.set(od.origin_name, newNode); //add it to the map
-            newLink.source = newNode;
-        }else{
-            const existingNode = nodesData.get(od.origin_name);
-            
-            existingNode.outgoing.push(newLink);
-            existingNode.outgoingTotal += newLink.value;
-            newLink.source = existingNode;
-        }
-        
-        if(!nodesData.get(od.dest_name)){
-            const newNode = {
-                name: od.dest_name,
-                incoming: [newLink],
-                outgoing: [],
-                incomingTotal: newLink.value,
-                outgoingTotal: 0
-            };
-            nodesData.set(od.dest_name,newNode);
+            nodesData.set(d.schcode_dest,newNode);
             newLink.target = newNode;
         }else{
-            const existingNode = nodesData.get(od.dest_name);
-            existingNode.incoming.push(newLink);
-            existingNode.incomingTotal += newLink.value;
+            const existingNode = nodesData.get(d.schcode_dest);
+            existingNode.totalEnters += newLink.value;
             newLink.target = existingNode;
+        };
+        
+        if(!nodesData.get(d.schcode_origin)){
+            const newNode = {
+                schcode: d.schcode_origin,
+                xy: d.xy_origin,
+                totalEnters: 0
+            };
+            nodesData.set(d.shcode_origin,newNode);
+            newLink.source = newNode;
+        }else{
+            const existingNode = nodesData.get(d.schcode_origin);
+            newLink.source = existingNode;
         }
-        
-        linksData.push(newLink);
-        
-        //if present, then modify the existing node
-        //if not present, create a new node
-    });*/
+        linksData.push(newLink);  
+    })
+    
+    //console.log(nodesData);
+    //console.log(linksData);
+    
+    return[nodesData,linksData];
+    
+}
 
 
+
+
+
+//This isn't actually the bar chart; it's just the dots to practicing displaying this for now.
 
 function drawBarChart(rootDom,data){
     
@@ -269,7 +265,10 @@ function drawBarChart(rootDom,data){
     
 }
 
-function drawMap(rootDom,data){
+
+// returns xy, added to the objects for an array data, with ojbect property lndLat, baed on rootDom specifications
+
+function myProjection(rootDom,data){
     const w = rootDom.clientWidth;
     const h = rootDom.clientHeight;
     
@@ -288,17 +287,46 @@ function drawMap(rootDom,data){
         return d.lngLat[1];
     })
     
-    //console.log(minLng);
-    //console.log(maxLng);
+    const projection = d3.geoMercator()
+        .scale(45000)
+        .center([(maxLng+minLng)/2,(maxLat+minLat)/2+.2])
+        //.center(289,127)
+        .translate([w/2,h/2]);
     
-    //console.log(projection_tm([minLng,minLat]));
-    //console.log(projection_tm([maxLng,maxLat]));
+    //console.log(data);
     
-    /*const scaleX = d3.scaleLinear()
-        .domain([projection_tm([minLng,minLat])[0],projection_tm([maxLng,maxLat])[0]])
-        .range ([5,w-5]);*/
+    data.forEach(d=>
+                 {d.xy = projection(d.lngLat);
+                 }
+    );
+    //console.log(data);
+    return(data);
     
-    //console.log(scaleX(289));
+}
+
+
+
+
+///First map - just locations
+
+function drawMap(rootDom,data){
+    const w = rootDom.clientWidth;
+    const h = rootDom.clientHeight;
+    
+    const projection_tm = d3.geoMercator()
+    
+    const minLng = d3.min(data, function(d){
+        return d.lngLat[0];
+    })
+    const maxLng = d3.max(data, function(d){
+        return d.lngLat[0];
+    })
+    const minLat = d3.min(data, function(d){
+        return d.lngLat[1];
+    })
+    const maxLat = d3.max(data, function(d){
+        return d.lngLat[1];
+    })
     
     const projection = d3.geoMercator()
         .scale(45000)
@@ -340,90 +368,51 @@ function drawMap(rootDom,data){
 
 
 
-/*//Import all data via parallel promises
-Promise.all([
-		migrationDataPromise,
-		countryCodePromise,
-		metadataPromise
-	]).then(([migration, countryCode, metadata]) => {  
+//// original nodes and links work - it was in the main promise section
 
-		//YOUR CODE HERE
-		//Nest/group migration_2000 by origin_country
-		//Then sum up the total value, using either nest.rollup or array.map
-		let migration_origin_by_country = d3.nest()
-        .key(d => d.origin_name)
-        .entries(migration_2000)
-        .map( d=>{
-            return{
-                origin_name: d.key,
-                total: d3.sum(d.values, e => e.value)
-            }
-        });
-        //console.log(migration_origin_by_country);
-        //COMPLETE HERE
-
-		//YOUR CODE HERE
-		//Then, join the transformed migration data to the lngLat values in the metadata
-
-
-		//REPRESENT
-		drawCartogram(d3.select('.cartogram').node(), migration_origin_by_country_aug);
-
-	})
-
-//YOUR CODE HERE
-//Complete the drawCartogram function
-//Some of the functions related to geographic representation have already been implemented, so feel free to use them
-function drawCartogram(rootDom, data){
-
-	//measure the width and height of the rootDom element
-	const w = rootDom.clientWidth;
-	const h = rootDom.clientHeight;
-
-	//projection function: takes [lng, lat] pair and returns [x, y] coordinates
-	const projection = d3.geoMercator()
-		.translate([w/2, h/2]);
-
-	//Scaling function for the size of the cartogram symbols
-	//Assuming the symbols are circles, we use a square root scale
-	const scaleSize = d3.scaleSqrt().domain([0,1000000]).range([5,50]);
-
-	//Complete the rest of the code here
-	//Build the DOM structure using enter / exit / update
+    /*enters1718.forEach(d => {
+        const newLink ={
+            value:d.enters
+        }
+        
+        if(!nodesData18.get(d.schcode_dest)){ //undefined is false
+            const newNode = {
+                schcode: d.schcode_dest
+                //incoming: [newLink],
+                //incomingTotal: newLink.value
+            };
+            
+            nodesData18.set(d.schcode_dest, newNode); //add it to the map
+            newLink.dest = newNode
+            ;
+        }else{
+            const existingNode = nodesData18.get(d.schcode_dest);
+            //existingNode.schcode = d.schcode_dest,
+            //existingNode.incoming.push(newLink);
+            //existingNode.incomingTotal += newLink.value;
+            newLink.dest = existingNode;
+        }
+        
+        if(!nodesData18.get(d.schcode_origin)){
+            const newNode = {
+                schcode: d.schcode_origin
+            };
+            
+            nodesData18.set(d.schcode_origin, newNode);
+            newLink.origin = newNode;
+        }else{
+            const existingNode = nodesData18.get(d.schcode_origin);
+            //existingNode.schcode = 
+            newLink.origin = existingNode;
+        }
+        
+        
+        linksData18.push(newLink);
+        
+        //if present, then modify the existing node
+        //if not present, create a new node
+    })*/
     
-    const plot = d3.select(rootDom)
-        .append('svg') //adds svg element in main then full-width-catogram
-        .attr('width', w)
-        .attr('height', h)
-        .append('g'); //adds g element in svg
-    
-    const nodes = plot.selectAll('.node')
-        .data(data,d => d.key); //not sure what this does
-    const nodesEnter = nodes.enter().append ('g')
-        .attr('class','node'); //now there's a g node for each origin country
-    nodesEnter.append('circle');
-    nodesEnter.append('text').attr('text-anchor','middle');
-    //now each node has circle and text elements
-    
-    nodes.merge(nodesEnter)
-		.filter(d => d.lngLat)
-		.attr('transform', d => {
-			const xy = projection(d.origin_lngLat);
-			return `translate(${xy[0]}, ${xy[1]})`;
-		})//not sure what this does
-    nodes.merge(nodesEnter)
-		.select('circle')
-		.attr('r', d => scaleSize(d.total))
-		.style('fill-opacity', .03)
-		.style('stroke', '#000')
-		.style('stroke-width', '1px')
-		.style('stroke-opacity', .2) //gives the circle visual elements
-    nodes.merge(nodesEnter)
-		.select('text')
-		.filter(d => d.value > 1000000)
-		.text(d => d.origin_name)
-		.style('font-family', 'sans-serif')
-		.style('font-size', '10px')
+    //console.log(nodesData18);
+    //console.log(linksData18);
 
-
-}*/
