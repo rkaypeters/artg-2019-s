b32159bc2ff721586eb894a7e7a511c52fea15b0
Rachel <rkaypeters@gmail.com>
2019-04-05 13:56:47 -0400

    pre-Siqi meeting

--- week-1/part-3-data-structures/script.js
+++ week-1/part-3-data-structures/script.js
@@ -1,3 +1,5 @@
+//I'm not sure what happened with this. I remember doing the homework, but most likely I messed up with github. Or maybe I did it in the wrong folder or saved incorrectly. Anyway, here it is now.
+
 //You'll show messages in the console using console.log
 console.log("Week-1 part-3: data structures");
 
@@ -15,22 +17,72 @@ const data = [
 //Using array.forEach, print out (using console.log) the names of the 4 cities, followed by their population.
 //The message should have the following sample format
 //"seattle, WA has a population of 652405"
+
+console.log('#1:');
+
 data.forEach(function(d){
 	//YOUR CODE HERE
+    console.log(d.city + ', ' + d.state + ' has a population of '+ d.population);
 });
 
 //#2
 //Using array.forEach to sum up the populations of the 4 cities
 //and print out the average population of the 4 cities
 
+console.log('#2:');
+
+var totPop = 0;
+
+data.forEach(d =>
+             {totPop += d.population;}
+            );
+
+console.log('average poulation: ' + totPop/data.length);
+
 //#3
 //Sort these 4 cities in terms of land area, from highest to lowest
 //And print out the name of the city with the largest land area
 //Hint: use array.sort
 
+console.log('#3:');
+
+data.sort((a,b)=>
+         {return (b.land_area - a.land_area);}
+         );
+
+console.log(data[0].city);
+
+
 //#4
 //Using array.map, compute the population density of these 4 cities (population divided by area)
 //add population density as a property to each object, and return the array
 
+//I probably wouldn't use density for this... maybe I don't fully understand all of the uses for map; I tried and didn't see a way to use it to add a property to elements in an existing array.
+
+console.log('#4:');
+
+const density = data.map(d => d.population/d.land_area);
+
+console.log(density);
+
+const density2 = data.map(d=>
+    {return{city: d.city,
+            state: d.state,
+            population: d.population,
+            land_area: d.land_area,
+            density: d.population/d.land_area
+           }
+});
+
+console.log(density2);
+
+
+
 //#5
 //Using array.filter, return a subset of the cities with a population <1 million
+
+console.log('#5:');
+
+const subLT1M = data.filter( d => d.population < 1000000);
+
+console.log(subLT1M);
